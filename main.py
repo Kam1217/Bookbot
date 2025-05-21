2fe9c6c472e064416ad34faa490a2488ebed712e
Kam1217 <kamilaostroga@gmail.com>
2025-05-21 19:36:43 +0100

    Add comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#Open and read book file
 def get_book_text(filepath):
     with open(filepath) as book_file:
         return book_file.read()
 
+#Print book file to consol
 def main():
     print(get_book_text("./books/frankenstein.txt"))
     
-
 main()
