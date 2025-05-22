aae84b281420757e10bfdcd70978dddf18273a6e
Kam1217 <kamilaostroga@gmail.com>
2025-05-22 14:22:46 +0100

    Refactoring to use multiple python files

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import count_words
+
 #Open and read book file
 def get_book_text(filepath):
     with open(filepath) as book_file:
         return book_file.read()
 
-#Count number of words
-def count_words(text):
-    word_count = len(text.split())
-    return word_count
-    
 #Print book words count to consol
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,5 @@
+#Count number of words
+def count_words(text):
+    word_count = len(text.split())
+    return word_count
+    
